816965005c8d349c2a271d57d15800d3f0ead074
samarth1011-1 <145792280+samarth1011-1@users.noreply.github.com>
2026-06-21 11:17:46 +0530

    tcl script

---
+++ run.tcl
@@ -0,0 +1,16 @@
+set rtl_files [glob -nocomplain rtl/*.v]
+
+# for any specific file change rtl/*.v to rtl/<file_name>.v
+
+set tb_files [glob -nocomplain tb/*.v]
+
+# for any specific file change tb/*.v to tb/<file_name>.v
+
+set all_files [concat $rtl_files $tb_files]
+
+eval exec iverilog -o output $all_files
+
+set sim_out [eval vvp output]
+
+puts $sim_out
+
